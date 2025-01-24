8c20daabb1f911f2b7ea678214e923d8b74c1cc1
AMAN PANDEY <119959836+Aman782@users.noreply.github.com>
2025-01-24 21:42:52 +0530

    Update Signup.jsx

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -18,7 +18,7 @@ const Signup = ({setLoggedIn}) => {
     e.preventDefault();
       
     try {
-      const response = await axios.post('https://tradebazaar-9a8c.onrender.com/users/register-user', {
+      const response = await axios.post('https://tradebazaarbackend.onrender.com/users/register-user', {
         username,
         email,
         password,
