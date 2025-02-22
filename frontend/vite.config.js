02a939c58578fc1e9a5d41142f52dd3cd440422c
Aman Pandey <amanpandey45692@gmail.com>
2025-02-22 19:30:45 +0530

    again vite.js updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', 
   optimizeDeps: {
-    include: ['youtube-player']
+    include: ['react-youtube', 'youtube-player']
   }
 });
