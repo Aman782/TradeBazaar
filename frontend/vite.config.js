1fafad2b0cfb9764bb60968e5922d3762fd386e4
Aman Pandey <amanpandey45692@gmail.com>
2025-02-22 19:25:37 +0530

    vite.js update done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: './', 
-  build: {
-    rollupOptions: {
-      external: ['youtube-player']
-    }
-  } 
+  optimizeDeps: {
+    include: ['youtube-player']
+  }
 });
